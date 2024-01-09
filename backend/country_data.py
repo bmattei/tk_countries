eb2f315e2755f3556d9d397ef198f04bfa0fba71
bmattei <bobmattei@gmail.com>
2024-01-09 16:00:16 -0500

    Changed code just to see if workflow works

--- backend/country_data.py
+++ backend/country_data.py
@@ -7,7 +7,7 @@ class CountryData:
     _instance = None
     _data_loaded = False
 
-    # add comment just to trigger the workflow
+    # add comment to trigger workflow
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(CountryData, cls).__new__(cls)
